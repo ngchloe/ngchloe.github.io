a910f59faddc64849f7ab0fa8aee2faf9587557e
ngchloe <109575115+ngchloe@users.noreply.github.com>
2022-07-22 00:48:22 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+function calculate() {
+    var panel = parseInt(document.getElementById("panel").value),
+        panelv = 65,
+        panelt = panel * panelv,
+        derating_value = 2,
+        total_hours_standby = panelt * derating_value;
 
+    document.getElementById("total_hours").innerHTML = total_hours_standby;
+}
