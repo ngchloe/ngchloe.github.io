57914ea848ca8b8e027e252dc79ccda10dce7c5e
ngchloe <109575115+ngchloe@users.noreply.github.com>
2022-07-22 00:10:07 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-function calculate(){
-        var A = document.getElementById("a").value;
-        var B = document.getElementById("b").value;
-        var C = document.getElementById("c").value;
-        var D = document.getElementById("d").value;
 
-        var ans = ((A * B) - (C * D)) / (C - B);
-        alert(ans);
-      }
